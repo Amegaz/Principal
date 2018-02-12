c0319f70afe90333edad6294d95d8ad1a84971fb
unknown <ame_96@live.com.mx>
2018-02-11 20:43:35 -0600

    Modificado, Cadenas

--- Lexico.py
+++ Lexico.py
@@ -4,227 +4,201 @@ sentences = otro.split('\n')
 
 def valLexico(letter, state):
     if state == 'Inicial':
-        state = valMinus(letter)
-    elif state == 'Identificador':
-        state = valCero(letter)
+        return valMinus(letter)
+    elif state == 'Identificador' or state == 'if' or state == 'while' or state == 'return' or state == 'else' or state == 'tipo':
+        return valZero(letter)
     elif state == 'Entero':
-        state = valOne(letter)
+        return valOne(letter)
     elif state == 'Real':
-        state = valTwo(letter)
-    elif state == 'int':
-        state = valFourInt(letter)
-    elif state == 'i':
-        state = valNineten(letter)
-    elif state == 'float1':
-        state = valFourF(letter)
-    elif state == 'float2':
-        state = valFourFl(letter)
-    elif state == 'float3':
-        state = valFourFlo(letter)
-    elif state == 'float4':
-        state = valFourFloa(letter)
-    elif state == 'float':
-        state = valCero(letter)
-    elif state == 'void1':
-        state = valFourV(letter)
-    elif state == 'void2':
-        state = valFourVo(letter)
-    elif state == 'void3':
-        state = valFourVoi(letter)
-    elif state == 'void':
-        state = valCero(letter)
-    elif state == 'OpSuma' or state == 'OpMul' or state == 'OpOr' or state == 'OpAnd' or state == 'if':
-        state = 'Cadena'
-    elif state == state == ';' or state == ',' or state == '(' or state == ')' or state == '{' or state == '}':
-        state = 'Cadena'
+        return valTwo(letter)
+    elif state == 'CadenaIncompleta':
+        return valThree(letter)
     elif state == 'OpRelac':
-        state = valSeven(letter)
-    elif state == 'OpOr1':
-        state = valEight(letter)
-    elif state == 'OpAnd1':
-        state = valNine(letter)
-    elif state == 'OpNot':
-        state = valCero(letter)
-    elif state == 'while1':
-        state = valTwentyW(letter)
-    elif state == 'while2':
-        state = valTwentyWh(letter)
-    elif state == 'while3':
-        state = valTwentyWhi(letter)
-    elif state == 'while4':
-        state = valTwentyWhil(letter)
-    elif state == 'while':
-        state = valCero(letter)
-    elif state == 'return1':
-        state = valTwentyoneR(letter)
-    elif state == 'return2':
-        state = valTwentyoneRe(letter)
-    elif state == 'return3':
-        state = valTwentyoneRet(letter)
-    elif state == 'return4':
-        state = valTwentyoneRetu(letter)
-    elif state == 'return5':
-        state = valTwentyoneRetur(letter)
-    elif state == 'return':
-        state = valCero(letter)
-    elif state == 'else1':
-        state = valTwentytwoE(letter)
-    elif state == 'else2':
-        state = valTwentytwoEl(letter)
-    elif state == 'else3':
-        state = valTwentytwoEls(letter)
-    elif state == 'else':
-        state = valCero(letter)
-    return state
+        return valSeven(letter)
+    elif state == 'OpOrIncompleto':
+        return valEight(letter)
+    elif state == 'OpAndIncompleto':
+        return valNine(letter)
+    elif state == '=' or state == 'OpNot':
+        return valEleven(letter)
+    elif state == 'i':
+        return valI(letter)
+    elif state == 'w':
+        return valW(letter)
+    elif state == 'wh':
+        return valWh(letter)
+    elif state == 'whe':
+        return valWhe(letter)
+    elif state == 'wher':
+        return valWher(letter)
+    elif state == 'r':
+        return valR(letter)
+    elif state == 're':
+        return valRe(letter)
+    elif state == 'ret':
+        return valRet(letter)
+    elif state == 'retu':
+        return valRetu(letter)
+    elif state == 'retur':
+        return valRetur(letter)
+    elif state == 'e':
+        return valE(letter)
+    elif state == 'el':
+        return valEl(letter)
+    elif state == 'els':
+        return valEls(letter)
+    elif state == 'in':
+        return valIn(letter)
+    elif state == 'f':
+        return valF(letter)
+    elif state == 'fl':
+        return valFl(letter)
+    elif state == 'flo':
+        return valFlo(letter)
+    elif state == 'floa':
+        return valFloa(letter)
+    elif state == 'v':
+        return valV(letter)
+    elif state == 'vo':
+        return valVo(letter)
+    elif state == 'voi':
+        return valVoi(letter)
+    else:
+        return 'Invalido'
 
-def valTwentytwoEls(letter):
-    if letter == 'e':
-        return 'else'
-    else:
-        return valCero(letter)
-
-def valTwentytwoEl(letter):
-    if letter == 's':
-        return 'else3'
-    else:
-        return valCero(letter)
-
-def valTwentytwoE(letter):
-    if letter == 'l':
-        return 'else2'
-    else:
-        return valCero(letter)
-
-def valTwentyoneRetur(letter):
-    if letter == 'n':
-        return 'return'
-    else:
-        return valCero(letter)
-def valTwentyoneRetu(letter):
-    if letter == 'r':
-        return 'return5'
-    else:
-        return valCero(letter)
-def valTwentyoneRet(letter):
-    if letter == 'u':
-        return 'return4'
-    else:
-        return valCero(letter)
-    
-def valTwentyoneRe(letter):
-    if letter == 't':
-        return 'return3'
-    else:
-        return valCero(letter)
-def valTwentyoneR(letter):
-    if letter == 'e':
-        return 'return2'
-    else:
-        return valCero(letter)
-
-def valTwentyWhil(letter):
-    if letter == 'e':
-        return 'while'
-    else:
-        return valCero(letter)
-
-def valTwentyWhi(letter):
-    if letter == 'l':
-        return 'while4'
-    else:
-        return valCero(letter)
-
-def valTwentyWh(letter):
-    if letter == 'i':
-        return 'while3'
-    else:
-        return valCero(letter)
-
-def valTwentyW(letter):
-    if letter == 'h':
-        return 'while2'
-    else:
-        return valCero(letter)
-
-def valNineten(letter):
+def valI(letter):
     if letter == 'f':
         return 'if'
     elif letter == 'n':
-        return 'int'
+        return 'in'
     else:
-        return valCero(letter)
+        return valZero(letter)
 
-def valSeven(letter):
-    if letter == '=' and len(sentence) == 2:
-        return 'OpRelac'
-    else:
-        return valCero(letter)
-
-def valEight(letter):
-    if letter == '|':
-        return 'OpOr'
-    else:
-        return valCero(letter)
-
-def valNine(letter):
-    if letter == '&':
-        return 'OpAnd'
-    else:
-        return valCero(letter)
-
-def valFourVoi(letter):
-    if letter == 'd':
-        return 'void'
-    else:
-        return valCero(letter)
-
-def valFourVo(letter):
-    if letter == 'i':
-        return 'void3'
-    else:
-        return valCero(letter)
-
-def valFourV(letter):
-    if letter == 'o':
-        return 'void2'
-    else:
-        return valCero(letter)
-
-def valFourFloa(letter):
+def valIn(letter):
     if letter == 't':
-        return 'float'
+        return 'tipo'
     else:
-        return valCero(letter)
+        return valZero(letter)
 
-def valFourFlo(letter):
-    if letter == 'a':
-        return 'float4'
-    else:
-        return valCero(letter)
-
-def valFourFl(letter):
-    if letter == 'o':
-        return 'float3'
-    else:
-        return valCero(letter)
-
-def valFourF(letter):
+def valF(letter):
     if letter == 'l':
-        return 'float2'
+        return 'fl'
     else:
-        return valCero(letter)
+        return valZero(letter)
 
-def valFourInt(letter):
-    if letter == 't' and len(sentence) == 3:
-        return 'int'
+def valFl(letter):
+    if letter == 'o':
+        return 'flo'
     else:
-        return valCero(letter)
+        return valZero(letter)
 
-def valTwo(letter):
-    if letter.isdigit():
-        return 'Real'
+def valFlo(letter):
+    if letter == 'a':
+        return 'floa'
     else:
-        return 'Cadena'
+        return valZero(letter)
+
+def valFloa(letter):
+    if letter == 't':
+        return 'tipo'
+    else:
+        return valZero(letter)
+
+def valV(letter):
+    if letter == 'o':
+        return 'vo'
+    else:
+        return valZero(letter)
+
+def valVo(letter):
+    if letter == 'i':
+        return 'voi'
+    else:
+        return valZero(letter)
+
+def valVoi(letter):
+    if letter == 'd':
+        return 'tipo'
+    else:
+        return valZero(letter)
+
+def valW(letter):
+    if letter == 'h':
+        return 'wh'
+    else:
+        return valZero(letter)
+
+def valWh(letter):
+    if letter == 'e':
+        return 'whe'
+    else:
+        return valZero(letter)
+
+def valWhe(letter):
+    if letter == 'r':
+        return 'wher'
+    else:
+        return valZero(letter)
+    
+def valWher(letter):
+    if letter == 'e':
+        return 'where'
+    else:
+        return valZero(letter)
+
+def valR(letter):
+    if letter == 'e':
+        return 're'
+    else:
+        return valZero(letter)
+
+def valRe(letter):
+    if letter == 't':
+        return 'ret'
+    else:
+        return valZero(letter)
+
+def valRet(letter):
+    if letter == 'u':
+        return 'retu'
+    else:
+        return valZero(letter)
+
+def valRetu(letter):
+    if letter == 'r':
+        return 'retur'
+    else:
+        return valZero(letter)
+
+def valRetur(letter):
+    if letter == 'n':
+        return 'return'
+    else:
+        return valZero(letter)
+
+def valE(letter):
+    if letter == 'l':
+        return 'el'
+    else:
+        return valZero(letter)
+
+def valEl(letter):
+    if letter == 's':
+        return 'els'
+    else:
+        return valZero(letter)
+
+def valEls(letter):
+    if letter == 'e':
+        return 'else'
+    else:
+        return valZero(letter)
+
+def valZero(letter):
+    if letter.isalpha() or letter.isdigit():
+        return 'Identificador'
+    else:
+        return 'Invalido'
 
 def valOne(letter):
     if letter.isdigit():
@@ -232,55 +206,79 @@ def valOne(letter):
     elif letter == '.':
         return 'Real'
     else:
-        return 'Cadena'
+        return 'Invalido'
 
-def valCero(letter):
-    if letter.isalpha() or letter.isdigit():
-        return 'Identificador'
-    elif letter == '+' or letter == '-' or letter == '*' or letter == '/' or letter == ';' or letter == ',' or letter == '.' or letter == '(' or letter == ')' or letter == '{' or letter == '}' or letter == '<' or letter == '>' or letter == '=' or letter == '!':
+def valTwo(letter):
+    if letter.isdigit():
+        return 'Real'
+    else:
+        return 'Invalido'
+
+def valThree(letter):
+    if letter == '"':
         return 'Cadena'
     else:
-        return 'No reconocido'
+        return 'CadenaIncompleta'
 
+def valSeven(letter):
+    if letter == '=':
+        return 'OpRelac'
+    else:
+        return 'Invalido'
+
+def valEight(letter):
+    if letter == '|':
+        return 'OpOr'
+    else:
+        return 'Invalido'
+    
+def valNine(letter):
+    if letter == '&':
+        return 'OpAnd'
+    else:
+        return 'Invalido'
+
+def valEleven(letter):
+    if letter == '=':
+        return 'OpIgualdad'
+    else:
+        return 'Invalido'
+    
 def valMinus(letter):
     if letter == 'i':
         return 'i'
-    elif letter == 'f':
-        return 'float1'
-    elif letter == 'v':
-        return 'void1'
-    elif letter == 'w':
-        return 'while1'
-    elif letter == 'r':
-        return 'return1'
-    elif letter == 'e':
-        return 'else1'
-    elif letter.isalpha():
+    if letter == 'w':
+        return 'w'
+    if letter == 'r':
+        return 'r'
+    if letter == 'e':
+        return 'e'
+    if letter == 'f':
+        return 'f'
+    if letter == 'v':
+        return 'v'
+    elif letter.isalpha() or letter == '_':
         return 'Identificador'
     elif letter.isdigit():
         return 'Entero'
     elif letter == '.':
         return 'Real'
-    elif letter == '+':
+    elif letter == '"':
+        return 'CadenaIncompleta'
+    elif letter == '+' or letter == '-':
         return 'OpSuma'
-    elif letter == '-':
-        return 'OpSuma'
-    elif letter == '*':
+    elif letter == '*' or letter == '/':
         return 'OpMul'
-    elif letter == '/':
-        return 'OpMul'
-    elif letter == '<':
-        return 'OpRelac'
-    elif letter == '>':
+    elif letter == '<' or letter == '>':
         return 'OpRelac'
     elif letter == '|':
-        return 'OpOr1'
+        return 'OpOrIncompleto'
     elif letter == '&':
-        return 'OpAnd1'
-    elif letter == '!':#Se comparte con es distinto de !=
+        return 'OpAndIncompleto'
+    elif letter == '!':
         return 'OpNot'
     elif letter == '=':
-        return 'OpIgualdad'#Se comparte con el de igualar =
+        return '='
     elif letter == ';':
         return ';'
     elif letter == ',':
@@ -296,26 +294,15 @@ def valMinus(letter):
     elif letter == '$':
         return '$'
     else:
-        return 'No reconocido'
-
-def valChar(letter, state):
-    if letter.isalpha():
-        return 'Identificador'
-    elif letter.isdigit():
-        if state!='Real':
-            return 'Entero'
-        else:
-            return 'Real'
-    elif letter == '.':
-        return 'Real'
+        return 'Error'
 
 for sentence in sentences:
     i = 0
     typeDate = 'Inicial'
     while i < len(sentence):
         typeDate = valLexico(sentence[i], typeDate)
-        #typeDate = valChar(sentence[i], typeDate)
-        print(sentence[i], typeDate)
+        #if 'Invalido' in typeDate:
+        #    print('Invalido')
         i+=1
-    print(typeDate + "\n")
+    print(sentence + "\t" + typeDate)
 file.close()
